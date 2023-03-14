a1739ebf0aaa4b99fd7d487021c6779b24a0d5d5
MrVester0 <jekaterina.tkatsjova@tktk.ee>
2023-03-14 21:28:17 +0200

    add week7 code

---
+++ Week7Functions/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Are yo coming or leaving (in/out):");
+string userChoice = Console.ReadLine();
+
+if (userChoice == "in")
+{
+    PrintHello();
+}
+else
+{
+    PrintGoodBay();
+}
+
+PrintHello();
+PrintGoodBay();
+
+
+static void PrintHello()
+{
+    Console.WriteLine("Hello, World!");
+}
+
+static void PrintGoodBay()
+{
+    Console.WriteLine("See you latter!");
+}
